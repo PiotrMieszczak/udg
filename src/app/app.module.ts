817720264080f52f1d86a593d649af3abea32464
pmieszczak <piotr.mieszczak@syzygy.pl>
2022-05-13 23:21:42 +0200

    test: 💍 AppComponent added tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,25 @@
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { TuiRootModule, TuiDialogModule, TuiNotificationsModule } from "@taiga-ui/core";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TuiRootModule, TuiDialogModule } from '@taiga-ui/core';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HeaderModule} from "./layout/header/header.module";
+import { HeaderModule } from './layout/header/header.module';
 
 const APP_MODULES = [HeaderModule];
-const UI_LIB_MODULES = [TuiRootModule, TuiNotificationsModule, TuiDialogModule];
+const UI_LIB_MODULES = [TuiRootModule, TuiDialogModule];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     [...APP_MODULES],
-    [...UI_LIB_MODULES]
-],
+    [...UI_LIB_MODULES],
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
